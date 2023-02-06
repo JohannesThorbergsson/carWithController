e53a4036e761a7047cf7d0ca066ff2ef61d4e61e
Johannes Thorbergsson <johannes.thorbergsson@hotmail.de>
2023-02-06 15:07:31 +0100

    Initial commit

---
+++ src/main/java/com/example/carwithcontroller/CarWithControllerApplication.java
@@ -0,0 +1,13 @@
+package com.example.carwithcontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarWithControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarWithControllerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/carwithcontroller/CarWithControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carwithcontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarWithControllerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
